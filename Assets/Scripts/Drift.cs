87d817e03ded01e9ceb27368dfa051ee341bff28
Anthony Ilersich <antonio.ilersich@gmail.com>
2020-11-14 17:57:35 -0500

    basic asteroid movement

---
+++ Assets/Scripts/Drift.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drift : MonoBehaviour
+{
+	public Vector2 driftVelocity = Vector2.down;
+
+	void Start()
+	{
+	}
+
+	void Update()
+	{
+		transform.Translate(driftVelocity * Time.deltaTime);
+	}
+}
